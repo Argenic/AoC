8e7494b8e9ae1ffd13c1f63f4d6c55670a58efa3
Argenic <simon.zwaanenburg@gmail.com>
2021-12-04 10:14:13 +0100

    3.2 - Ugly solution

--- DayThree.java
+++ DayThree.java
@@ -18,7 +18,7 @@ public class DayThree {
     private LinkedList<String> lines = new LinkedList<>();
     
     /**
-     * Setup day two.
+     * Setup day three.
      */
     public DayThree() {
         try {
@@ -37,14 +37,13 @@ public class DayThree {
     }
     
     /**
-     * First part of day two.
+     * First part of day three.
      */
     public void partOne() {
         // Setup some vars
         int[] ones = new int[lines.get(0).length()];
         String gammaBinaryString = "";
         String epsilonBinaryString = "";
-        
         // Loop the lines
         for(String line : lines) {
             for(int i = 0 ; i < line.length() ; i++) {
@@ -73,17 +72,67 @@ public class DayThree {
         // Present the awnser.
         System.out.println(
             "2021 Day Three - Part One = Gamma:" + gamma + 
-                " * Epsilon:" + epsilon +
-                " = " + gamma * epsilon
+            " * Epsilon:" + epsilon +
+            " = " + gamma * epsilon
         );
     }
     
     /**
-     * Second part of day two.
+     * Second part of day three.
      */
     public void partTwo() {
+        // Setup some vars
+        LinkedList<String> generatorList = (LinkedList) lines.clone();
+        LinkedList<String> scrubberList = (LinkedList) lines.clone();
+        for(int i = 0 ; i < lines.get(0).length() ; i++) {
+            // Setup vars
+            LinkedList<String> zeroes = new LinkedList();
+            LinkedList<String> ones = new LinkedList();
+            // Loop all generatorLines left and split
+            for(String generatorLine : generatorList) {
+                if(generatorLine.charAt(i) == '0') {
+                    zeroes.add(generatorLine);
+                } else if (generatorLine.charAt(i) == '1') { 
+                    ones.add(generatorLine);
+                }
+            }
+            // Decide which list to replace our previous list with
+            if(ones.size() >= zeroes.size()) {
+                generatorList = ones;
+            } else {
+                generatorList = zeroes;
+            }
+            // Check if we have stuff left to check first
+            if(scrubberList.size() == 1) {
+                continue;
+            }
+            // Scrubber stuff
+            zeroes = new LinkedList();
+            ones = new LinkedList();
+            for(String scrubberLine : scrubberList) {
+                if(scrubberLine.charAt(i) == '0') {
+                    zeroes.add(scrubberLine);
+                } else if (scrubberLine.charAt(i) == '1') { 
+                    ones.add(scrubberLine);
+                }
+            }
+            // Decide on which list
+            if(zeroes.size() > ones.size()) {
+                scrubberList = ones;
+            } else if(zeroes.size() < ones.size()){
+                scrubberList = zeroes;
+            } else {
+                scrubberList = zeroes;
+            }
+        }
+        // Process results
+        int generator = Integer.parseInt(generatorList.getFirst(), 2);
+        int scrubber = Integer.parseInt(scrubberList.getFirst(), 2);
+        
         System.out.println(
-            "2021 Day Three - Part Two = "
+            "2021 Day Three - Part Two = Generator:" + generator + 
+            " * Scrubber:" + scrubber +
+            " = " + generator * scrubber
         );
     }
 }
